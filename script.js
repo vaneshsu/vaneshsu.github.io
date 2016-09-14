4ecc780d96e4172dfd80ddcfce384c4a963f9b8d
vaneshsu <vaneshsu@gmail.com>
2016-09-14 04:10:12 -0700

    First commit
    website

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "150px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
