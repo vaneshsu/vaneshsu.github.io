d7cb711eaf1f694b9b193c0a9a555396ebd137d0
vaneshsu <vaneshsu@gmail.com>
2017-04-27 11:15:41 -0700

    responsive side nav
    fixed responsive side nav to stay across entire page

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 /**
  * 
  */
+(function() {
+	var $ = function(id) { return document.getElementById(id); }
+	window.onload = function() {
+		$("close-nav").onclick = closeNav;
+		$("open-nav").onclick = openNav;
+	};
+	function openNav() {
+		$("open-nav").style.display = "none";
+	    $("mySidenav").style.width = "100%";
+	    $("mySidenav").style.height = "10%";
+	}
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
+	function closeNav() {
+		$("open-nav").style.display = "unset";
+	    $("mySidenav").style.width = "0";
+	    $("mySidenav").style.height = "0";
+	}
+})();
